3bc1d26c9ab9efed14187b1c3580b2600256c403
Ryan Pickell <rianchristopher@gmail.com>
2022-09-12 22:23:55 -0700

    wrote chat imports and chat container tags in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import { Cookies } from 'universal-cookie';
+
+const apiKey = 'g7sz268nx38g';
+
+const client = StreamChat.getInstance(apiKey)
 
 const App = () => {
   return (
-    <div>
-      <h1>Message Board</h1>
+    <div className="app_wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer
+
+        />
+        <ChannelContainer
+          
+        />
+
+      </Chat>
     </div>
   );
 }
