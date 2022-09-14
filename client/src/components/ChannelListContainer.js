2f94a85ef62f76d288e79bb37e5ab8522eb4b62e
Ryan Pickell <rianchristopher@gmail.com>
2022-09-13 23:03:16 -0700

    added initial sidebar form

--- client/src/components/ChannelListContainer.js
+++ client/src/components/ChannelListContainer.js
@@ -1,4 +1,18 @@
 import React from 'react';
+import { ChannelList, useChatContext } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
+
+const SideBar = () => (
+  <div className="channel-list__sidebar">
+    <div className="channel-list__sidebar__icon1">
+      <div className="icon1__inner">
+
+      </div>
+    </div>
+  </div>
+)
 
 const ChannelListContainer = () => {
   return (
