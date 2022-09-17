d3e12691016458e11f8586890bed6899ba38ae1b
Ryan Pickell <rianchristopher@gmail.com>
2022-09-16 18:44:29 -0700

    added onclick functionality for channel preview

--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -25,8 +25,16 @@ const TeamChannelPreview = ({ channel, type }) => {
   }  
 
   return (
-    <div>
+    <div className={
+      channel?.id === activeChannel?.id
+      ? 'channel-preview__wrapper__selected'
+      : 'channel-preview__wrapper'
+    }
+    onClick={() => {
+      console.log(channel);
 
+    }}>
+      {type === 'team' ? <ChannelPreview /> : <DirectPreview />}
     </div>
   )
 }
