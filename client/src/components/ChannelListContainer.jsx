fc4998a4cff76af2411843372b1fd874d3f8d707
Ryan Pickell <rianchristopher@gmail.com>
2022-09-16 17:15:12 -0700

    added team channel list props

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -38,6 +38,15 @@ const ChannelListContainer = () => {
       <div className="channel-list__list__wrapper">
         <CompanyHeader />
         <ChannelSearch />
+        <ChannelList
+          filters={{}}
+          channelRenderFilterFn={() => {}}
+          List={(listProps) => (
+            <TeamChannelList 
+              { ... listProps}
+            />
+          )}
+        />  
       </div>
     </>
   );
---
+++ client/src/components/TeamChannelList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { AddChannel } from '../assets';
+
+const TeamChannelList = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default TeamChannelList
