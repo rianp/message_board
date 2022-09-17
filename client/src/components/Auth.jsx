f4d3c562f299ee564f951d539a895ba2e5b4d06b
Ryan Pickell <rianchristopher@gmail.com>
2022-09-17 16:44:10 -0700

    added sign up/in container field

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -2,10 +2,20 @@ import React, { useState } from 'react';
 import Cookies from 'universal-cookie';
 import axios from 'axios';
 
+import signinImage from '../assets/signup.jpg';
+
 const Auth = () => {
+  const [isSignup, setIsSignup] = useState(false);
+
   return (
     <div>
-
+      <div className="auth__form-container">
+        <div className="auth__form-container_fields">
+          <div className="auth__form-container_fields-content">
+            <p>{isSignup ? 'Sign Up' : 'Sign In'}</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
